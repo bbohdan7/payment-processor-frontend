427ce723fb66674a6abf9f2d55306e5c37129c5e
Bogdan <zbohdan7@gmail.com>
2020-11-09 23:40:04 +0200

    changed URL for backend for clusterIP

--- src/http-common.jsx
+++ src/http-common.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://192.168.0.103:32555/payment-processor/api/",
+    baseURL: "http://payment-processor-backend:9080/payment-processor/api/",
     headers: {
         "Content-Type": "application/json"
     }
