48b495083bd3ef75b16d82d02b7f19770ea6285d
Bogdan <zbohdan7@gmail.com>
2021-10-03 09:57:13 +0000

    Changed backend destination

--- src/http-common.jsx
+++ src/http-common.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://192.168.0.131:30920/payment-processor/api/",
+    baseURL: "http://192.168.0.139:9080/payment-processor/api/",
     headers: {
         "Content-Type": "application/json"
     }
